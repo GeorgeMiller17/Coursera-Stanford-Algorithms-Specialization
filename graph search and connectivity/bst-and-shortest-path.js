3eaf8264a3b30673658f4d60a3680ffc827d9ddc
zhenghao <iron4wine@outlook.com>
2018-04-08 23:29:49 -0700

    update

--- graph search and connectivity/bst-and-shortest-path.js
+++ graph search and connectivity/bst-and-shortest-path.js
@@ -50,6 +50,16 @@ class Graph {
     return [];
   }
 
+  dfs(start,results = []) {
+    results.push(start);
+    if (this.graph[start]) {
+    for (let node of this.graph[start]) {
+      if (results.indexOf(node)===-1) this.dfs(node,results);
+      }
+    }
+    return results;
+  }
+
 }
 
 
@@ -63,7 +73,9 @@ g.addEdge('4', '6')
 g.addEdge('5', '6')
 
 console.log(g)
-var bfsPath = g.bfs('1')
-console.log(bfsPath);
-var shortestPath = g.shortestPath(1,5);
-console.log(shortestPath);
+// var bfsPath = g.bfs('1')
+// console.log(bfsPath);
+// var shortestPath = g.shortestPath(1,5);
+// console.log(shortestPath);
+var dfsPath = g.dfs('1');
+console.log(dfsPath);
