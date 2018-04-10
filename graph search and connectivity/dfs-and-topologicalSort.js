db86c845b31854de6ab779503d7c52a95e80f128
zhenghao <iron4wine@outlook.com>
2018-04-09 19:01:50 -0700

    update

--- graph search and connectivity/dfs-and-topologicalSort.js
+++ graph search and connectivity/dfs-and-topologicalSort.js
@@ -1,25 +1,14 @@
-class Graph {
+class graph {
   constructor() {
-    this.graph = {};
+    this.edges = {};
     this.vertices = [];
   }
 
   addEdge(u,v) {
     this.vertices.indexOf(u)===-1 && this.vertices.push(u);
     this.vertices.indexOf(v)===-1 && this.vertices.push(v);
-    this.graph[u] ? this.graph[u].push(v) : this.graph[u] = [v];
+    this.edges[u] ? this.edges[u].push(v) : this.edges[u] = [v];
   }
-
-  dfs(start,results = []) {
-    results.push(start);
-    if (this.graph[start]) {
-    for (let node of this.graph[start]) {
-      if (results.indexOf(node)===-1) this.dfs(node,results);
-      }
-    }
-    return results;
-  }
-
 
   topologicalSorting() {
     let dfs, vertices, visitedVertices, results;
@@ -28,7 +17,7 @@ class Graph {
     dfs = (vertex) => {
       let neighbors;
       visitedVertices[vertex] = true;
-      neighbors = this.graph[vertex];
+      neighbors = this.edges[vertex];
       if (neighbors) {
         for (let neighbor of neighbors) {
           if (!visitedVertices[neighbor]) dfs(neighbor);
@@ -37,35 +26,21 @@ class Graph {
       results.unshift(vertex);
     }
     for (let vertex of this.vertices) {
-      if (!visitedVertices[vertex]) dfs(vertex);
+      if (!visitedVertices[vertex]) {
+        dfs(vertex);}
     }
     return results;
   }
 }
 
 
-let g = new Graph()
-// g.addEdge('s', 'v')
-// g.addEdge('s', 'w')
-// g.addEdge('v', 't')
-// g.addEdge('w', 't')
-
-// g.addEdge('1', '4')
-// g.addEdge('1', '2')
-// g.addEdge('2', '4')
-// g.addEdge('2', '3')
-// g.addEdge('4', '3')
-// g.addEdge('4', '5')
-// g.addEdge('3', '5')
-g.addEdge(5, 2);
-g.addEdge(5, 0);
-g.addEdge(4, 0);
-g.addEdge(4, 1);
-g.addEdge(2, 3);
-g.addEdge(3, 1);
-// g.addEdge('4', '0')
-// g.addEdge('4', '1')
-console.log(g)
-
-var t = g.topologicalSorting();
-console.log(t);
+// let g = new graph()
+// g.addEdge('C', 'E');
+// g.addEdge('D', 'E');
+// g.addEdge('B', 'C');
+// g.addEdge('C', 'D');
+// g.addEdge('A', 'B');
+// g.addEdge('A', 'D');
+// console.log(g);
+// var t = g.topologicalSorting();
+// console.log(t);
